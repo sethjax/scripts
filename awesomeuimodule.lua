291023345e453fd3a5408268972dadc3313e1a1d
sethjax <107009717+sethjax@users.noreply.github.com>
2025-08-08 12:45:44 -0700

    Update awesomeuimodule.lua

--- awesomeuimodule.lua
+++ awesomeuimodule.lua
@@ -10,23 +10,23 @@ function module.GetScreenMiddleCoordinates(): Vector2
 end
 
 function module.CreateOption(holderFrame:Frame)
-	
+
 end
 
 function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nameProperty:string, defaultPosition:Vector2, defaultSize: Vector2, backgroundFrameProperties: {}, themeColor:Color3}): {NewScreenGui: ScreenGui, BackgroundFrame: Frame, insertObject: BindableFunction, removeObject: BindableFunction}
 	local NewScreenGui = Instance.new("ScreenGui")
 	NewScreenGui.Name = dat.nameProperty or "genericWindow"
 	NewScreenGui.IgnoreGuiInset = true
-	
+
 	local BackgroundFrame = Instance.new("CanvasGroup", NewScreenGui)
 	BackgroundFrame.Size = UDim2.fromOffset(dat.defaultSize.X,dat.defaultSize.Y)
 	BackgroundFrame.Position = UDim2.fromOffset(dat.defaultPosition.X, dat.defaultPosition.Y)
 	if not dat.backgroundFrameProperties.BackgroundColor3 then
 		BackgroundFrame.BackgroundColor3 = Color3.new(0.0941176, 0.0941176, 0.0941176)
 	end
-	
+
 	local themeColor = dat.themeColor or Color3.new(0, 0.568627, 1)
-	
+
 	for i,v in pairs(dat.backgroundFrameProperties or {}) do
 		local success, err = pcall(function()
 			BackgroundFrame[i] = v
@@ -35,22 +35,22 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 			warn("Error applying property: ", err, "(backgroundFrameProperties)")
 		end
 	end
-	
+
 	local UICorner = Instance.new("UICorner", BackgroundFrame)
-	
+
 	local Stroke = Instance.new("UIStroke", BackgroundFrame)
 	Stroke.Color = BackgroundFrame.BackgroundColor3
-	
+
 	local Gradient = Instance.new("UIGradient", BackgroundFrame)
 	Gradient.Color = ColorSequence.new(Color3.new(1, 1, 1), Color3.new(0.65098, 0.65098, 0.65098))
 	Gradient.Rotation = 90
-	
+
 	--toolbar------------------------------------------------------------------------------------------------toolbar
 	local topbarFrameSize = 25
 	local toolbarSize = 14
-	
+
 	local topbarFrame = Instance.new("Frame", BackgroundFrame)
-	
+
 	local DragDetector = Instance.new("UIDragDetector", topbarFrame)
 	DragDetector.ResponseStyle = Enum.UIDragDetectorResponseStyle.CustomOffset
 
@@ -59,45 +59,45 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 		startDragOffset = Vector2.new(BackgroundFrame.AbsolutePosition.X - inputPosition.X, BackgroundFrame.AbsolutePosition.Y - inputPosition.Y)
 		startDragOffset += Vector2.new(0,57)
 	end)
-	
+
 	DragDetector.DragContinue:Connect(function(inputPosition)
 		BackgroundFrame.Position = UDim2.fromOffset(startDragOffset.X + inputPosition.X, startDragOffset.Y + inputPosition.Y)
 	end)
-	
+
 	topbarFrame.Interactable = true
 	topbarFrame.Active = true
 	topbarFrame.BackgroundColor3 = themeColor
-	
+
 	local topbarHolder = Instance.new("Frame", topbarFrame)
-	
+
 	topbarHolder.BackgroundTransparency = 1
 	topbarHolder.Size = UDim2.new(1,-8,0,topbarFrameSize-5)
 	topbarHolder.AnchorPoint = Vector2.new(.5,0)
 	topbarHolder.Position = UDim2.new(0.5,0,0,4)
-	
+
 	local leftButtons = topbarHolder:Clone()
 	leftButtons.Size = UDim2.new(1,0,1,0)
-	
+
 	local leftbtnListLayout = Instance.new("UIListLayout", leftButtons)
 	leftbtnListLayout.Padding = UDim.new(0,5)
 	leftbtnListLayout.FillDirection = Enum.FillDirection.Horizontal
 	leftbtnListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Left
 	leftbtnListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
-		
+
 	local title = Instance.new("TextLabel", leftButtons)
 	title.BackgroundTransparency = 1
-	
+
 	title.Size = UDim2.new(1,-10,1,-10)
 	title.Text = dat.nameProperty or "Window"
-	
+
 	title.TextSize = 14
 	title.TextYAlignment = Enum.TextYAlignment.Center
 	title.TextXAlignment = Enum.TextXAlignment.Left
 	title.Font = Enum.Font.RobotoMono
-	
+
 	local textsize = TextService:GetTextSize(title.Text, title.TextSize, title.Font, Vector2.new(10000,10000))
 	title.Size = UDim2.new(0,textsize.X,0,textsize.Y)
-	
+
 	local rightButtons = topbarHolder:Clone()
 	rightButtons.Size = UDim2.new(1,0,1,0)
 
@@ -106,7 +106,7 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 	rightbtnListLayout.FillDirection = Enum.FillDirection.Horizontal
 	rightbtnListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
 	rightbtnListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
-	
+
 	leftButtons.Parent = topbarHolder
 	rightButtons.Parent = topbarHolder
 
@@ -124,9 +124,9 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 	topbarFrame.Size = UDim2.new(1,0,0,topbarFrameSize+3+toolbarSize+3)
 	topbarFrame.Position = UDim2.fromOffset(0,0)
 	topbarFrame.BackgroundTransparency = 0
-	
+
 	--------------------------------------------------------------------
-	
+
 	local toolbarFrame = Instance.new("Frame", BackgroundFrame)
 
 	local corner = Instance.new("UICorner", toolbarFrame)
@@ -140,8 +140,8 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 	ListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Left
 	ListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
 	ListLayout.Padding = UDim.new(0,5)
-	
-	
+
+
 	toolbarFrame.Size = UDim2.new(1,-8,0,toolbarSize)
 	toolbarFrame.Position = UDim2.fromOffset(4,topbarFrameSize+3)
 	toolbarFrame.BackgroundColor3 = BackgroundFrame.BackgroundColor3
@@ -156,15 +156,15 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 		newbutton.TextSize = toolbarSize-2
 		newbutton.Font = Enum.Font.RobotoMono
 		newbutton.TextColor3 = themeColor
-		
+
 		local hoverHighlight = Instance.new("UIStroke")
 		hoverHighlight.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 		hoverHighlight.Color = themeColor
-		
+
 		newbutton.MouseEnter:Connect(function()
 			hoverHighlight.Parent = newbutton
 		end)
-		
+
 		newbutton.MouseLeave:Connect(function()
 			hoverHighlight.Parent = nil
 		end)
@@ -189,16 +189,16 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 	end
 
 	--toolbar------------------------------------------------------------------------------------------------toolbar
-	
+
 	local insertObject = Instance.new("BindableFunction")
-	
+
 	insertObject.OnInvoke = function(dat: {Type:string})
 		if not dat.Type then
 			warn("No type specified")
 			return
 		end
 		if dat.Type == "Toolbar" then
-			
+
 		elseif dat.Type == "Page" then
 			local holderFrame = Instance.new("Frame",BackgroundFrame)
 			holderFrame.AnchorPoint = Vector2.new(.5,1)
@@ -209,35 +209,35 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 
 			local corner = Instance.new("UICorner", holderFrame)
 			corner.CornerRadius = UDim.new(0,10)
-			
+
 			local Scroller = Instance.new("ScrollingFrame", holderFrame)
 			Scroller.Size = UDim2.new(1,0,1,0)
 			Scroller.ScrollBarThickness = 0
 			Scroller.BackgroundTransparency = 1
-			
+
 			local ListLayout = Instance.new("UIListLayout", Scroller)
 			ListLayout.FillDirection = Enum.FillDirection.Vertical
 			ListLayout.Padding = UDim.new(0,3)
-			
+
 			ListLayout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
 				Scroller.CanvasSize = UDim2.new(0,0,0,ListLayout.AbsoluteContentSize.Y)
 			end)
-			
+
 			local Padding = Instance.new("UIPadding", Scroller)
 			Padding.PaddingLeft = UDim.new(0,4)
 			Padding.PaddingRight = UDim.new(0,4)
 			Padding.PaddingTop = UDim.new(0,4)
 			Padding.PaddingBottom = UDim.new(0,4)
-			
+
 			return holderFrame, Scroller
 		elseif dat.Type == "Option" then
 			local newOptionFrame = Instance.new("Frame")
 			newOptionFrame.Size = UDim2.new(1,0,0,23)
 			newOptionFrame.BackgroundColor3 = Color3.new(0.223529, 0.203922, 0.215686)
-			
+
 			local corner = Instance.new("UICorner", newOptionFrame)
 			corner.CornerRadius = UDim.new(0,4)
-			
+
 			local newLabel = Instance.new("TextLabel", newOptionFrame)
 			newLabel.Position = UDim2.fromOffset(7,0)
 			newLabel.Font = Enum.Font.RobotoMono
@@ -247,9 +247,9 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 			newLabel.TextSize = 13
 			newLabel.TextXAlignment = Enum.TextXAlignment.Left
 			newLabel.TextYAlignment = Enum.TextYAlignment.Center
-			
+
 			newLabel.Text = dat.Name or "Option (no name?)"
-			
+
 			local textBox = Instance.new("TextBox", newOptionFrame)
 			textBox.AnchorPoint = Vector2.new(1,.5)
 			textBox.Position = UDim2.new(1,-2,.5,0)
@@ -259,28 +259,28 @@ function module.CreateWindow(dat: {isDraggable:boolean, isResizable:boolean, nam
 			textBox.Text = ""
 			textBox.TextSize = 13
 			textBox.ClearTextOnFocus = false
-			
+
 			local corner = Instance.new("UICorner", textBox)
 			corner.CornerRadius = UDim.new(0,2)
-			
+
 			if dat.onvaluechangeCallback then
 				textBox.FocusLost:Connect(function()
 					dat.onvaluechangeCallback(textBox.Text)
 				end)
 			end
-			
+
 			textBox.Font = Enum.Font.RobotoMono
 			textBox.PlaceholderText = "Input value..."
-			
+
 			return newOptionFrame
 		end
 	end
-	
+
 	local removeObject = Instance.new("BindableEvent")
-	
-	
+
+
 	NewScreenGui.Parent = Player.PlayerGui
-	
+
 	return {NewScreenGui = NewScreenGui, BackgroundFrame = BackgroundFrame, insertObject = insertObject, removeObject = removeObject, insertToolbarButton = InsertToolbarButton}
 end
 
